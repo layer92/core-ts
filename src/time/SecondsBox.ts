af52750111a788a94bfc6deb06124a744182e742
layer92 <user@example.com>
2024-05-15 16:47:57 +0900

    bug fix and toTypicalHoursMinutesSecondsString

--- src/time/SecondsBox.ts
+++ src/time/SecondsBox.ts
@@ -1,4 +1,5 @@
 import { Box } from "../away/Box";
+import { PadLeft } from "../strings/Strings";
 
 export class SecondsBox extends Box<number>{
     
@@ -48,6 +49,9 @@ export class SecondsBox extends Box<number>{
         if(seconds < 60){
             unit = "second";
             value = seconds;
+        }else if(this.toMinutes()<60){
+            unit = "minute";
+            value = Math.floor(this.toMinutes());
         }else if(this.toDays()<1){
             unit = "hour";
             value = Math.floor(this.toHours());
@@ -55,12 +59,32 @@ export class SecondsBox extends Box<number>{
             unit = "day";
             value = Math.floor(this.toDays());
         }
-        const isPlural = value>1;
+        const isPlural = value>1||value===0;
         if(isPlural){
             unit += `s`;
         }
         return `${value} ${unit}`;
     }
+
+    /**
+     *  Returns hh:mm:ss, omitting hours if empty, not omitting minutes if not empty
+     *  This is the same format found in several places, such as on Wikipedia when listing album length and track length.
+     */
+    toTypicalHoursMinutesSecondsString(){
+        const hours = Math.floor(this.toHours());
+        const minutes = Math.floor(this.toMinutes()) - hours*60;
+        const seconds = Math.floor(this.getData()) - minutes*60;
+        let result = PadLeft(""+seconds,"0",2);
+        if(hours>0){
+            result = hours+":"+PadLeft(""+minutes,"0",2)+":"+result;
+        }else if(minutes>0){
+            result = minutes+":"+result;
+        }else{
+            result = "0:"+result;
+        }
+        return result;
+    }
+
 
     static FromMilliseconds(ms:number){
         return new SecondsBox(ms/1000);
