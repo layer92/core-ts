cb199413fbc81968a9096143db2fb117a2bc3354
layer92 <user@example.com>
2026-01-05 19:17:35 -0800

    bugfix GetBaseNameFromFileName

--- src/files/FileNames.test.ts
+++ src/files/FileNames.test.ts
@@ -1,9 +1,10 @@
 import assert from "assert";
-import { MaybeGetFormatFromFileName } from "./FileNames";
+import { GetBaseNameFromFileName, MaybeGetFormatFromFileName } from "./FileNames";
 
 export function TestFileNames() {
     console.log("\t FileNames");
     TestMaybeGetFormatFromFileName();
+    TestGetBaseNameFromFileName();
 }
 
 function TestMaybeGetFormatFromFileName(){
@@ -14,6 +15,18 @@ function TestMaybeGetFormatFromFileName(){
         ["foo.",undefined],
     ] as const){
         const result = MaybeGetFormatFromFileName(input);
+        assert.equal(result,expected,`Failed: input:${input===undefined?`undefined`:`"${input}"`} expected:${expected===undefined?`undefined`:`"${expected}"`} result:${result===undefined?`undefined`:`"${result}"`}`);
+    }
+}
+
+function TestGetBaseNameFromFileName(){
+    console.log(`\t\t .GetBaseNameFromFileName`);
+    for(const [input,expected] of [
+        ["foo.bar","foo"],
+        ["foo","foo"],
+        ["foo.","foo."],
+    ] as const){
+        const result = GetBaseNameFromFileName(input);
         assert.equal(result,expected,`Failed: input:${input===undefined?`undefined`:`"${input}"`} expected:${expected===undefined?`undefined`:`"${expected}"`} result:${result===undefined?`undefined`:`"${result}"`}`);
     }
 }
